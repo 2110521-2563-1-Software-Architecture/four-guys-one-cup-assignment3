7f2fa78d73bbb8740c6688c7fd9cc97ae525c3e3
wattanaitsp <wattanai@nsquared.asia>
2020-09-23 02:45:37 +0700

    init: finish MVC

---
+++ mvc/mvc/controllers/main_controller.py
@@ -0,0 +1,22 @@
+from mvc.models.repositories.note_repository import NoteRepository
+
+class MainController:
+    def __init__(self):
+        # Create note repository here
+        # Your code here
+        self.note_repository = NoteRepository()
+    
+    def get_all_notes(self):
+        # Return all notes
+        # Your code here
+        return self.note_repository.get_all_notes()
+
+    def add_note(self, note: str):
+        # Add note
+        # Your code here
+        self.note_repository.add_note(note)
+
+    def clear_all(self):
+        # Clear all note
+        # Your code here
+        self.note_repository.clear_all_notes()
